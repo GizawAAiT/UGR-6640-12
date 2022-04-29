9b864bc68d981d93a858035bff8c9a4d9ff12256
GizawAAiT <gizawaait@gmail.com>
2022-04-29 04:30:24 +0300

    portfolio updated

---
+++ Update/js/script.js
@@ -0,0 +1,6 @@
+var typed =new Typed(".typing",{
+    strings:["I'm ","Frontend developer,","Mobile Programer, ","Graphics Designer"," and Problem Solver."],
+    typeSpeed:100,
+    BackSpeed:100,
+    loop:true,
+})
